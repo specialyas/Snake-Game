20b40075fe3679e1dfff77afd029d476362a4a2d
specialyas <54225481+specialyas@users.noreply.github.com>
2021-09-08 03:35:55 +0100

    Detected Collision with the wall
    The game will exit anytime the snake hits the wall

--- main.py
+++ main.py
@@ -33,4 +33,8 @@ while game_is_on:
         scoreboard.increase()
         food.refresh()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 screen.exitonclick()
