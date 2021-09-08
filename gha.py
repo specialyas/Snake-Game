6d2c2a1d3d95bd9c82c01cd9dc5ac666fcc9be5d
specialyas <54225481+specialyas@users.noreply.github.com>
2021-09-08 02:51:54 +0100

    added a a scoreboard functionality
    The score will increase any time the snake eats the food

---
+++ gha.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+
+
+turtle = Turtle()
+turtle.goto(0, 200)
+turtle.write("Home = ", align="center", font=("Arial", 12, "normal"))
+
+
+segment = Turtle(shape='turtle')
+
+
+
+screen = Screen()
+screen.exitonclick()
---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.right, 'Right')
+screen.onkey(snake.left, 'Left')
+
+food = Food()
+
+scoreboard = Scoreboard()
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        scoreboard.increase()
+        food.refresh()
+
+screen.exitonclick()
